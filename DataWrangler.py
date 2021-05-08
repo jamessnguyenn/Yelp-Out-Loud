9f27d0dcbb33f205680e31382527490fa2368289
James Nguyen <nguyen.james615@gmail.com>
2021-05-08 11:31:45 -0700

    deleted test files and addded data wrangler

---
+++ DataWrangler.py
@@ -0,0 +1,20 @@
+userFile = open("yelp_academic_dataset_user.json", encoding = 'UTF-8')
+newUserFile = open("user.json", 'a',  encoding = 'UTF-8')
+counter = 450000
+for line in userFile:
+    newUserFile.write(line)
+    counter-=1
+    if(counter == 0):
+        break
+newUserFile.close()
+userFile.close()
+reviewFile = open("yelp_academic_dataset_review.json", encoding = 'UTF-8')
+newReviewFile = open("review.json", 'a',  encoding = 'UTF-8')
+counter = 1500000
+for line in reviewFile:
+    newReviewFile.write(line)
+    counter-=1
+    if(counter==0):
+        break
+reviewFile.close()
+newUserFile.close()
--- Test.py
+++
@@ -1,8 +0,0 @@
-import datetime
-import uuid
-#For Testing statements to not effect the database
-
-
-#test datetime
-x = uuid.uuid4().hex
-print(str(x)[0:3]+'-'+str(x)[3:21])
